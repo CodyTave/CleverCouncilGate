355244709f1431c75795074604438ec11ba220cf
CodyTave <dahiroussama@gmail.com>
2023-08-09 11:37:03 +0100

    09/08

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import "./App.css";
-import { mfAca, mfCom, mfTech } from "./assets";
+import { aca, com, mfAca, mfCom, mfTech, tech } from "./assets";
 import { useState } from "react";
 import Title from "./components/Title";
+import { AnimatePresence, motion } from "framer-motion";
 function App() {
   const Clevers = [
     {
@@ -9,33 +10,63 @@ function App() {
       title: "COM & EVENTS",
       position: "translate-x-full",
       image: mfCom,
+      logo: com,
       bg: "bg-com-2",
       stroke: "stroke-com",
       text: "text-com-0",
+      color: "bg-com-0",
     },
     {
       id: "academy",
       title: "ACADEMY",
       position: "",
       image: mfAca,
+      logo: aca,
       bg: "bg-aca-2",
       stroke: "stroke-aca",
       text: "text-aca-1",
+      color: "bg-aca-1",
     },
     {
       id: "tech",
       title: "TECH SERVICES",
       position: "-translate-x-full",
       image: mfTech,
+      logo: tech,
       bg: "bg-tech-2",
       stroke: "stroke-tech",
       text: "text-tech-1",
+      color: "bg-tech-1",
     },
   ];
 
   const [selectedClever, setClever] = useState(Clevers[1]);
   return (
     <div className={`w-screen h-screen ${selectedClever.bg} transall`}>
+      <AnimatePresence>
+        <motion.img
+          key={selectedClever.logo}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.8, ease: [0.215, 0.61, 0.355, 1] }}
+          className="absolute sm:top-20 sm:left-28 top-10 left-10 w-20 sm:w-36 transall"
+          src={selectedClever.logo}
+        />
+      </AnimatePresence>
+      <div className="absolute transall md:right-1/2 sm:top-20  sm:right-28 sm:-translate-x-1/2 top-8 right-10 flex gap-3 items-center ">
+        {Clevers.map((clev) => (
+          <div
+            onClick={() => setClever(clev)}
+            key={clev.id}
+            className={` transall w-[1px] cursor-pointer ${
+              selectedClever.id === clev.id
+                ? `h-10 ${clev.color}`
+                : "h-7 bg-[#2F3165]"
+            }`}
+          ></div>
+        ))}
+      </div>
       <div className="flex xl:gap-14 h-full justify-center items-center  transall">
         {Clevers.map((clev) => (
           <img
@@ -55,8 +86,8 @@ function App() {
           className={`
           font-extrabold transall ${selectedClever.text} absolute tracking-widest
           xl:text-9xl  mmd:text-8xl sm:text-5xl xxs:text-3xl tn:text-xl text-sm truncate
-         lg:bottom-28 md:bottom-[20%] mmd:bottom-[18%] sm:bottom-[34%] msm:bottom-[38%] xxs:bottom-[41%] tn:bottom-[43%] bottom-[46%]
-         lg:left-48 md:left-36 mmd:left-24 sm:left-20 msm:left-20 xxs:left-10 tn:left-5 left-3
+         xl:bottom-48 lg:bottom-28 md:bottom-[20%] mmd:bottom-[18%] sm:bottom-[34%] msm:bottom-[38%] xxs:bottom-[41%] tn:bottom-[43%] bottom-[46%]
+         xl:left-60 lg:left-48 md:left-36 mmd:left-24 sm:left-20 msm:left-20 xxs:left-10 tn:left-5 left-3
             `}
         >
           CLEVER
@@ -65,7 +96,7 @@ function App() {
         <div
           className={`xl:text-9xl  mmd:text-8xl sm:text-5xl xxs:text-3xl tn:text-xl text-sm z-50
           absolute tracking-wider transall font-extrabold overflow-hidden truncate
-         lg:top-28 md:top-[20%] mmd:top-[18%] sm:top-[34%] msm:top-[38%] xxs:top-[41%] tn:top-[43%] top-[46%]
+         xl:top-48 lg:top-28 md:top-[20%] mmd:top-[18%] sm:top-[34%] msm:top-[38%] xxs:top-[41%] tn:top-[43%] top-[46%]
          lg:right-48 md:right-36 mmd:right-20 sm:right-28 msm:right-20 xxs:right-10 tn:right-5 right-3
           `}
         >
--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,21 +1,93 @@
-// import { gsap } from "gsap";
+import { gsap } from "gsap";
 import { useEffect, useState } from "react";
 
 interface props {
   prtitle: string;
   Style: string;
 }
-function Title({ prtitle, Style }: props) {
-  const [title, setTitle] = useState(prtitle);
+function Title({ prtitle }: props) {
+  const [prev, setPrev] = useState(prtitle);
   useEffect(() => {
-    // gsap.to(".title", { y: 0 });
-    // gsap.to(".title", { duration: 1, y: -100 });
-    setTitle(prtitle);
+    if (prtitle === "ACADEMY") {
+      const tl = gsap.timeline({ onComplete: () => setPrev(prtitle) });
+      if (prev === "COM & EVENTS") {
+        tl.to(".com", {
+          duration: 0.8,
+          ease: "cubic-bezier(0.215, 0.61, 0.355, 1)",
+          y: "-100%",
+        });
+        tl.to(".tech", { duration: -1, y: "-300%" });
+      } else {
+        tl.to(".tech", {
+          duration: 0.8,
+          ease: "cubic-bezier(0.215, 0.61, 0.355, 1)",
+          y: "-300%",
+        });
+        tl.to(".com", { duration: -1, y: "-100%" });
+      }
+      tl.to(".aca", {
+        duration: 0.8,
+        ease: "cubic-bezier(0.215, 0.61, 0.355, 1)",
+        y: "-100%",
+      });
+    } else if (prtitle === "COM & EVENTS") {
+      const tl = gsap.timeline({ onComplete: () => setPrev(prtitle) });
+      if (prev === "ACADEMY") {
+        tl.to(".aca", {
+          duration: 0.8,
+          ease: "cubic-bezier(0.215, 0.61, 0.355, 1)",
+          y: "-200%",
+        });
+        tl.to(".tech", { duration: -1, y: "-300%" });
+      } else {
+        tl.to(".tech", {
+          duration: 0.8,
+          ease: "cubic-bezier(0.215, 0.61, 0.355, 1)",
+          y: "-300%",
+        });
+        tl.to(".aca", { duration: -1, y: "-200%" });
+      }
+      tl.to(".com", {
+        duration: 0.8,
+        ease: "cubic-bezier(0.215, 0.61, 0.355, 1)",
+        y: "0%",
+      });
+    } else if (prtitle === "TECH SERVICES") {
+      const tl = gsap.timeline({ onComplete: () => setPrev(prtitle) });
+      if (prev === "ACADEMY") {
+        tl.to(".aca", {
+          duration: 0.8,
+          ease: "cubic-bezier(0.215, 0.61, 0.355, 1)",
+          y: "-200%",
+        });
+        tl.to(".com", { duration: -1, y: "-100%" });
+      } else {
+        tl.to(".com", {
+          duration: 0.8,
+          ease: "cubic-bezier(0.215, 0.61, 0.355, 1)",
+          y: "-100%",
+        });
+        tl.to(".aca", { duration: -1, y: "-200%" });
+      }
+      tl.to(".tech", {
+        duration: 0.8,
+        ease: "cubic-bezier(0.215, 0.61, 0.355, 1)",
+        y: "-200%",
+      });
+    }
   }, [prtitle]);
 
   return (
     <>
-      <div className={`title ${Style} `}>{title}</div>
+      <div className={`com -translate-y-full text-right stroke-com `}>
+        COM & EVENTS
+      </div>
+      <div className={`aca  text-right -translate-y-[200%] stroke-aca `}>
+        ACADEMY
+      </div>
+      <div className={`tech  text-right -translate-y-[300%] stroke-tech  `}>
+        TECH SERVICES
+      </div>
     </>
   );
 }
